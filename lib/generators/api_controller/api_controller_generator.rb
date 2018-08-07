5457319b8c709a85f8652003666dc4a4054e0838
thmyrk <przemyslaw.piotrowski@atteo.com>
2018-08-07 07:45:20 +0200

    Nest schema file in a plural model name directory

--- lib/generators/api_controller/api_controller_generator.rb
+++ lib/generators/api_controller/api_controller_generator.rb
@@ -6,7 +6,7 @@ class ApiControllerGenerator < Rails::Generators::NamedBase
     template "controller.rb", "app/controllers/api/v1/#{plural_name}_controller.rb"
     actions.each do |action|
       @action = action
-      template "schema.rb", "app/schemas/#{plural_name}_#{action}_schema.rb"
+      template "schema.rb", "app/schemas/#{plural_name}/#{plural_name}_#{action}_schema.rb"
       template "use_case.rb", "app/use_cases/#{plural_name}/#{action}.rb"
     end
     template "presenter.rb", "app/presenters/#{singular_name}_presenter.rb"
